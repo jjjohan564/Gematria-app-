cc19231103857f6f4e2e32cd9e670919a60cf4c0
jjjohan564 <jjjohan564@icloud.com>
2025-02-26 17:07:10 +0100

    service-worker.js erstellen

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'elegant-gematria-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
